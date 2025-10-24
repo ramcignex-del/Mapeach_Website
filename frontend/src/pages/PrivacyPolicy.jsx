1c65425b87acdf0d73a80cca209fc79965afa2d2
ramcignex-del <ram.cignex@gmail.com>
2025-10-24 21:21:00 +0530

    Update PrivacyPolicy.jsx

--- frontend/src/pages/PrivacyPolicy.jsx
+++ frontend/src/pages/PrivacyPolicy.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { privacyPolicyData } from "../data/privacyContent.js";
 
 
-import React, { useState } from 'react';
-import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
 import { Textarea } from '../components/ui/textarea'
 
