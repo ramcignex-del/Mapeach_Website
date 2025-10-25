7f8f52e1604df490dadc609583fe1ee0f5cbf133
ramcignex-del <ram.cignex@gmail.com>
2025-10-25 21:51:54 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,7 +39,8 @@ export const Navbar = () => {
   const navLinks = [
     { path: '/', label: 'Home', type: 'link' },
     // Type 'menu' means it's a dropdown header, not a direct link, path is '#' for non-navigation
-    { path: '#', label: 'For Companies', type: 'menu', subLinks: companyDropdownLinks }, 
+//    { path: '#', label: 'For Companies', type: 'menu', subLinks: companyDropdownLinks }, 
+    { path: '/hire', label: 'For Companies', type: 'link', subLinks: companyDropdownLinks }, 
     { path: '/join', label: 'For Talents', type: 'link' },
     { path: '/about', label: 'About', type: 'link' },
     { path: '/contact', label: 'Contact', type: 'link' },
