32cc28cd69690b7a69fd5ba8ee7138a57ad6c9f0
ramcignex-del <ram.cignex@gmail.com>
2025-10-25 20:10:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,12 @@ import MapeachFAQ from "./pages/MapeachFAQ";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ScrollToTop from './components/ScrollToTop';
 
+// 👇 1. IMPORT NEW SECTOR COMPONENTS (excluding Information Technology, which uses 'Hire') 👇
+import HealthTech from './pages/companies/HealthTech';
+import Healthcare from './pages/companies/Healthcare';
+import Lifesciences from './pages/companies/Lifesciences';
+import ElectronicsCommunications from './pages/companies/ElectronicsCommunications';
+
 function App() {
   return (
     <div className="App">
@@ -20,13 +26,32 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/hire" element={<Hire />} />
+          {/* This route is preserved and now serves as the 'Information Technology' page. */}
+          <Route path="/hire" element={<Hire />} /> 
           <Route path="/join" element={<Join />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/MapeachFAQ" element={<MapeachFAQ />} />
           <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          
+          {/* 👇 2. ADD ROUTES FOR THE REMAINING 4 SECTORS 👇 */}
+          <Route 
+            path="/companies/healthtech" 
+            element={<HealthTech />} 
+          />
+          <Route 
+            path="/companies/healthcare" 
+            element={<Healthcare />} 
+          />
+          <Route 
+            path="/companies/lifesciences" 
+            element={<Lifesciences />} 
+          />
+          <Route 
+            path="/companies/electronics" 
+            element={<ElectronicsCommunications />} 
+          />
         </Routes>
         <Footer />
       </HashRouter>
