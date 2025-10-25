9ce6b33200aa49ceec7de3bb2ea21a59844ce65d
ramcignex-del <ram.cignex@gmail.com>
2025-10-25 12:29:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashrRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import Home from "./pages/Home";
