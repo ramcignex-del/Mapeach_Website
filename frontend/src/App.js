8f2b2b1405d135a66c308901d17a148f403567e4
ramcignex-del <ram.cignex@gmail.com>
2025-10-26 19:28:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import ScrollToTop from './components/ScrollToTop';
+
 import Home from "./pages/Home";
 import Hire from "./pages/Hire";
 import Join from "./pages/Join";
@@ -11,9 +13,8 @@ import Contact from "./pages/Contact";
 import Jobs from "./pages/Jobs";
 import MapeachFAQ from "./pages/MapeachFAQ";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-import ScrollToTop from './components/ScrollToTop';
 
-// 👇 1. IMPORT THE RENAMED COMPONENT AND ALL OTHER SECTOR COMPONENTS 👇
+// Sector Pages
 import InformationTechnology from './pages/companies/InformationTechnology';
 import HealthTech from './pages/companies/HealthTech';
 // import Healthcare from './pages/companies/Healthcare';
@@ -26,49 +27,28 @@ function App() {
       <HashRouter>
         <Navbar />
         <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
+        {/* Wrapper to prevent content being hidden behind fixed navbar */}
+        <div className="pt-16">
+          <Routes>
+            {/* Main Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/hire" element={<Hire />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/jobs" element={<Jobs />} />
+            <Route path="/MapeachFAQ" element={<MapeachFAQ />} />
+            <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
 
-                   
-          {/* 👇 2. UPDATED ROUTE for Information Technology (formerly /hire) */}
-          <Route 
-            path="/companies/informationtechnology" 
-            element={<InformationTechnology />} 
-          /> 
-          
-          {/* 👇 3. ROUTES FOR REMAINING 4 SECTORS */}
-          <Route 
-            path="/companies/healthtech" 
-            element={<HealthTech />} 
-          />
-       {/*     
-          <Route 
-            path="/companies/healthcare" 
-            element={<Healthcare />} 
-          />
-        */}
-          
-          <Route 
-            path="/companies/lifesciences" 
-            element={<Lifesciences />} 
-          />
-          <Route 
-            path="/companies/electronics" 
-            element={<ElectronicsCommunications />} 
-          />
-          {/* ⬆️ ALL SECTOR ROUTES ARE NOW UNDER /COMPANIES/ ⬆️ */}
-
-          {/* Existing main routes remain untouched */}
-          <Route path="/hire" element={<Hire />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/MapeachFAQ" element={<MapeachFAQ />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-          
-        </Routes>
+            {/* Sector Routes */}
+            <Route path="/companies/informationtechnology" element={<InformationTechnology />} />
+            <Route path="/companies/healthtech" element={<HealthTech />} />
+            {/* <Route path="/companies/healthcare" element={<Healthcare />} /> */}
+            <Route path="/companies/lifesciences" element={<Lifesciences />} />
+            <Route path="/companies/electronics" element={<ElectronicsCommunications />} />
+          </Routes>
+        </div>
         <Footer />
       </HashRouter>
     </div>
