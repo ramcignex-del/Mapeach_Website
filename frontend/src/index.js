35ee3d72e3566839371b9bfc7455d74487c53630
ramcignex-del <ram.cignex@gmail.com>
2025-10-30 16:42:34 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "@/index.css";
-import App from "@/App";
+import "./index.css";
+import App from "./App";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
 
+if (!container) {
+  throw new Error("Root element with id 'root' not found in the document.");
+}
+
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
