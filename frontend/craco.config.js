b0f9c09524776835adce678c4136a81892698491
ramcignex-del <ram.cignex@gmail.com>
2025-11-04 20:16:10 +0530

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,4 +1,3 @@
-// craco.config.js
 const path = require("path");
 require("dotenv").config();
 
@@ -6,16 +5,6 @@ module.exports = {
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
-    },
-    configure: (webpackConfig) => {
-      // Allow importing CSS from node_modules (slick-carousel)
-      webpackConfig.module.rules.push({
-        test: /\.css$/,
-        include: path.resolve(__dirname, "node_modules/slick-carousel"),
-        use: ["style-loader", "css-loader"],
-      });
-
-      return webpackConfig;
     },
   },
 };
