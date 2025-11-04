d451a0d66dd8cb81de33bad28ce8f3fa384c57e6
ramcignex-del <ram.cignex@gmail.com>
2025-11-04 20:45:34 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,8 +11,10 @@ import { companyStats, clientLogos, testimonials, problems, solutions, techDomai
 // Import react-slick and styles
 import Slider from "react-slick";
 // Import slick-carousel styles from public folder (safe for CRA)
-import "/slick/slick.css";
-import "/slick/slick-theme.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 
 const Home = () => {
